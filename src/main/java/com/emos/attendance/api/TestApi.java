ecad0cc84e8adfe6b56a746de9ad5ab689586e31
Hardy <1277146050@qq.com>
2021-05-02 08:36:49 +0800

    add swagger

--- src/main/java/com/emos/attendance/api/TestApi.java
+++ src/main/java/com/emos/attendance/api/TestApi.java
@@ -1,11 +1,15 @@
 package com.emos.attendance.api;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api("TestAPI")
 public class TestApi {
 
+    @ApiOperation("helllo world")
     @GetMapping("/")
     public String test() {
         return "hello world";
