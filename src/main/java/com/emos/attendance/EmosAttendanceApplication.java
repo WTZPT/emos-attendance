49a5e3c7c12b4acdecb93e4590b5ec0bf1825a9d
Hardy <1277146050@qq.com>
2021-05-03 11:38:35 +0800

    添加防范xss攻击logic

--- src/main/java/com/emos/attendance/EmosAttendanceApplication.java
+++ src/main/java/com/emos/attendance/EmosAttendanceApplication.java
@@ -2,8 +2,10 @@ package com.emos.attendance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class EmosAttendanceApplication {
 
     public static void main(String[] args) {
